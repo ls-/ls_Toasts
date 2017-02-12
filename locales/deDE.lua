b4559b660647d5acede2bf14cb13e7c5fb8adf10
Val Voronov <i.lightspark@gmail.com>
2017-02-12 13:52:30 +0700

    Updated translations.
    Kudos to Ithilrandir@Curse.

--- locales/deDE.lua
+++ locales/deDE.lua
@@ -1,4 +1,4 @@
-﻿-- Contributors: pas06@Curse
+﻿-- Contributors: pas06@Curse, Ithilrandir@Curse
 
 local _, addonTable = ...
 local L = addonTable.L
@@ -8,36 +8,33 @@ local _G = _G
 
 if _G.GetLocale() ~= "deDE" then return end
 
--- Toast
 L["ANCHOR"] = "Anker der Benachrichtigung"
-L["TRANSMOG_ADDED"] = "Vorlage hinzugefügt"
-L["TRANSMOG_REMOVED"] = "Vorlage entfernt"
-
--- Config General
--- L["SETTINGS_GENERAL_DESC"] = "Thome thettings, duh... |cffffd200They are saved per character.|r\\nI strongly recommend to |cffe52626/reload|r UI after you're done setting up the addon. Even if you opened and closed this panel without changing anything, |cffe52626/reload|r UI. |cffffd200By doing so, you'll remove this config entry from the system and prevent possible taints.|r"
--- L["ANCHOR_FRAME"] = "Anchor Frame"
--- L["APPEARANCE_TITLE"] = "Appearance"
--- L["COLOURS"] = "Colour Names"
--- L["COLOURS_TOOLTIP"] = "Colours item, follower names by quality, and world quest, mission titles by rarity."
+L["ANCHOR_FRAME"] = "Anker Rahmen"
+L["APPEARANCE_TITLE"] = "Aussehen"
+L["COLORS"] = "Farben"
+-- L["COLORS_TOOLTIP"] = "Colours item, follower names by quality, and world quest, mission titles by rarity."
+-- L["DND"] = "DND"
+-- L["DND_TOOLTIP"] = "Toasts in DND mode won't be displayed in combat, but will be queued up in the system instead. Once you leave combat, they'll start popping up."
 L["FADE_OUT_DELAY"] = "Ausblendungsverzögerung"
 L["GROWTH_DIR"] = "Ausbreitungsrichtung"
 L["GROWTH_DIR_DOWN"] = "Nach unten"
 L["GROWTH_DIR_LEFT"] = "Nach links"
 L["GROWTH_DIR_RIGHT"] = "Nach rechts"
 L["GROWTH_DIR_UP"] = "Nach oben"
--- L["PROFILE_DESC"] = "To save current settings as a default preset click the button below. This feature may be quite handy, if you use more or less same layout on many characters. This way you'll need to tweak fewer things. |cffffd200Please note that there can be only 1 preset. Hitting this button on a different character will overwrite existing template.|r"
-L["PROFILE_SAVE"] = "Voreinstellung speichern"
-L["PROFILE_TITLE"] = "Einstellungsübertragung"
--- L["PROFILE_WIPE"] = "Wipe Preset"
+-- L["PROFILE"] = "Profile"
+-- L["PROFILE_COPY_FROM"] = "Copy from:"
+-- L["PROFILE_CREATE_NEW"] = "Create New Profile"
+-- L["PROFILE_DELETE_CONFIRM"] = "Are you sure you want to delete |cffffffff%s|r profile?"
+-- L["PROFILE_RESET_CONFIRM"] = "Are you sure you want to reset |cffffffff%s|r profile?"
+-- L["PROFILES_TITLE"] = "Profiles"
 L["SCALE"] = "Skalierung"
-L["TOAST_NUM"] = "Anzahl der Benachrichtigungen"
-
--- Config Type
-L["SETTINGS_TYPE_LABEL"] = "Benachrichtigungstypen"
+-- L["SETTINGS_GENERAL_DESC"] = "Thome thettings, duh...\\nI strongly recommend to |cffe52626/reload|r UI after you're done setting up the addon. Even if you opened and closed this panel without changing anything, |cffe52626/reload|r UI. |cffffd200By doing so, you'll remove this config entry from the system and prevent possible taints.|r"
 -- L["SETTINGS_TYPE_DESC"] = "Moar thettings..."
--- L["DND"] = "DND"
--- L["DND_TOOLTIP"] = "Toasts in DND mode won't be displayed in combat, but will be queued up in the system instead. Once you leave combat, they'll start popping up."
+L["SETTINGS_TYPE_LABEL"] = "Benachrichtigungstypen"
 L["TEST"] = "Test"
+L["TOAST_NUM"] = "Anzahl der Benachrichtigungen"
+L["TRANSMOG_ADDED"] = "Vorlage hinzugefügt"
+L["TRANSMOG_REMOVED"] = "Vorlage entfernt"
 L["TYPE"] = "Typ"
 L["TYPE_ACHIEVEMENT"] = "Erfolg"
 L["TYPE_ARCHAEOLOGY"] = "Archäologie"
@@ -51,4 +48,4 @@ L["TYPE_LOOT_SPECIAL"] = "Beute (Spezial)"
 L["TYPE_LOOT_SPECIAL_TOOLTIP"] = "Benachrichtigung, die von besonderen Beuteereignissen wie gewonnene Würfe, legendäre Gegenstände, persönliche Beute etc.."
 L["TYPE_RECIPE"] = "Rezept"
 L["TYPE_TRANSMOG"] = "Transmogrifikation"
-L["TYPE_WORLD QUEST"] = "Weltquest"
+L["TYPE_WORLD_QUEST"] = "Weltquest"
