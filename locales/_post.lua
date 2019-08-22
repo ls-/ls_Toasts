36579259929cb12cfe74829843a0161baba886d0
ls- <i.lightspark@gmail.com>
2019-08-22 11:55:30 +0700

    Colour loss texts red

---
+++ locales/_post.lua
@@ -0,0 +1,12 @@
+-- Contributors: Gotxiko@GitHub
+
+local _, addonTable = ...
+local L = addonTable.L
+
+-- Lua
+local _G = getfenv(0)
+
+-- Mine
+-- These rely on custom strings
+L["YOU_LOST_RED"] = "|cffdc4436" .. L["YOU_LOST"] .. "|r"
+L["TRANSMOG_REMOVED_RED"] = "|cffdc4436" .. L["TRANSMOG_REMOVED"] .. "|r"
