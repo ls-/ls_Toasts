a1bf039932446fba0e047a3e1312f422b352b8ed
Val Voronov <i.lightspark@gmail.com>
2022-10-21 23:29:14 +0700

    Update CHANGELOG

--- core/changelog.lua
+++ core/changelog.lua
@@ -6,5 +6,10 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-TBD.
+- Added 10.0.0 support;
+- Added new "Info" panel to the config. It has links to various resourced including my Discord
+  server dedicated to LS: * addons;
+- Renamed "Recipe" toasts to "Profession". Added profession skill line toasts;
+- Updated "Collection" toasts;
+- Updated embeds.
 ]]
