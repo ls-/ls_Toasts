092baaf4acee5bd3693ddf2efef52a0746c8b709
Val Voronov <i.lightspark@gmail.com>
2022-11-03 15:47:17 +0700

    📜

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,18 @@
 # CHANGELOG
 
+## Version 100000.02
+
+- Added an option to show poor quality quality items via common loot toasts. Thanks to Faqar@GitHub.
+- Added leaf ornaments to achievement toasts. I can't re-enable Blizz achievement toasts, so as a
+  compromise I chose to make achievement toasts more unique. If you want them to stand out even more
+  create a separate anchor to display them.
+- Fixed an issue where repeatedly testing toasts and flushing the queue would sometimes lock up the
+  addon.
+- Fixed corrupt beautycase border texture. It worked, but baked-in shadows were messed up.
+- Updated Traditional Chinese translation. Translated by BNS333@Curse.
+
+![Imgur](https://i.imgur.com/yqJ7C6S.png)
+
 ## Version 100000.01
 
 - Added 10.0.0 support;
--- core/changelog.lua
+++ core/changelog.lua
@@ -6,10 +6,12 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-- Added 10.0.0 support;
-- Added new "Info" panel to the config. It has links to various resourced including my Discord
-  server dedicated to LS: * addons;
-- Renamed "Recipe" toasts to "Profession". Added profession skill line toasts;
-- Updated "Collection" toasts;
-- Updated embeds.
+- Added an option to show poor quality quality items via common loot toasts. Thanks to Faqar@GitHub.
+- Added leaf ornaments to achievement toasts. I can't re-enable Blizz achievement toasts, so as a
+  compromise I chose to make achievement toasts more unique. If you want them to stand out even more
+  create a separate anchor to display them.
+- Fixed an issue where repeatedly testing toasts and flushing the queue would sometimes lock up the
+  addon.
+- Fixed corrupt beautycase border texture. It worked, but baked-in shadows were messed up.
+- Updated Traditional Chinese translation. Translated by BNS333@Curse.
 ]]
