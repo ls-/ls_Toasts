e16ef92b29851cc58593a9c7e50d1efad6aa5d77
Val Voronov <i.lightspark@gmail.com>
2022-11-17 11:50:37 +0700

    📜

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # CHANGELOG
 
+## Version 100002.01
+
+- Added 10.0.2 support.
+- Fixed an issue where the addon would load the achievement UI too early which in turn could cause
+  errors.
+
 ## Version 100000.03
 
 - Fixed file loading order.
--- core/changelog.lua
+++ core/changelog.lua
@@ -6,12 +6,7 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-- Added an option to show poor quality quality items via common loot toasts. Thanks to Faqar@GitHub.
-- Added leaf ornaments to achievement toasts. I can't re-enable Blizz achievement toasts, so as a
-  compromise I chose to make achievement toasts more unique. If you want them to stand out even more
-  create a separate anchor to display them.
-- Fixed an issue where repeatedly testing toasts and flushing the queue would sometimes lock up the
-  addon.
-- Fixed corrupt beautycase border texture. It worked, but baked-in shadows were messed up.
-- Updated Traditional Chinese translation. Translated by BNS333@Curse.
+- Added 10.0.2 support.
+- Fixed an issue where the addon would load the achievement UI too early which in turn could cause
+  errors.
 ]]
