6fad4c800ae69e7d68e3dad101ab88bfc91f7e59
Val Voronov <i.lightspark@gmail.com>
2023-02-01 04:23:09 +0700

    📜

--- ls_Toasts/core/changelog.lua
+++ ls_Toasts/core/changelog.lua
@@ -6,12 +6,8 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-- Added an option to show poor quality quality items via common loot toasts. Thanks to Faqar@GitHub.
-- Added leaf ornaments to achievement toasts. I can't re-enable Blizz achievement toasts, so as a
-  compromise I chose to make achievement toasts more unique. If you want them to stand out even more
-  create a separate anchor to display them.
-- Added new "Info" panel to the config. It has links to various resourced including my Discord
-  server dedicated to LS: * addons.
-- Fixed corrupt beautycase border texture. It worked, but baked-in shadows were messed up.
-- Updated Traditional Chinese translation. Translated by BNS333@Curse.
+- Added 3.4.1 support.
+- Updated French translation. Translated by Braincell1980@Curse.
+- Updated German translation. Translated by MrKimab@Curse.
+- Updated Traditional Chinese translation. Translated by RainbowUI@Curse.
 ]]
