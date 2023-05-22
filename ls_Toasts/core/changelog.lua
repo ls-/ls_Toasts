0024410853bd8f2d66f1276087612eae469500b1
Val Voronov <i.lightspark@gmail.com>
2023-05-22 10:09:12 +0700

    📜

--- ls_Toasts/core/changelog.lua
+++ ls_Toasts/core/changelog.lua
@@ -6,5 +6,7 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-- Fixed world quest toasts.
+- Added "Two Tone" variants to all built-in skins. They have a two tone background that's similar to that of Blizz
+  achievement toasts. I might tweak the textures depending on the feedback.
+- Updated Spanish translation. Translated by Shacoulrophobia@Curse.
 ]]
