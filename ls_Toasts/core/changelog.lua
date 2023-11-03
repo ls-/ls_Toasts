39b9cf468a0475a188dcf7b136a59434af2af395
Val Voronov <i.lightspark@gmail.com>
2023-11-03 20:07:53 +0700

    📜

--- ls_Toasts/core/changelog.lua
+++ ls_Toasts/core/changelog.lua
@@ -6,4 +6,13 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
+- Added 1.14.4 support;
+- Added "Two Tone" variants to all built-in skins. They have a two tone background that's similar
+  to that of Blizz achievement toasts.
+- Added an option to show toasts for poor quality items. 
+- Added an option to whitelist all pets.
+- Added an option to whitelist items by their IDs.
+- Added new "Info" panel to the config. It has links to various resources including my Discord
+  server dedicated to LS: * addons.
+- Numerous bug fixes and tweaks.
 ]]
