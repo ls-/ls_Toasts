acf1787428fb17815395e22e93c2bb1d2682b6ad
Val Voronov <i.lightspark@gmail.com>
2024-03-21 15:15:32 +0700

    📜

--- ls_Toasts/core/changelog.lua
+++ ls_Toasts/core/changelog.lua
@@ -6,10 +6,5 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-### Loot (Special)
-
-- Added loot roll info. It's a returning feature. However, there's a caveat, I don't know if "transmog" rolls are
-  actually a thing in this particular case because it's basically an undocumented feature. I added it based on my
-  assumptions, so if it works, good, if it doesn't, oh well. But "need", "greed", and "disenchant" rolls should work as
-  expected.
+- Added 10.2.6 support.
 ]]
