9972d9b72134edc1f8cefeaaaddf888e172e0946
Val Voronov <i.lightspark@gmail.com>
2024-09-12 23:12:18 +0700

    📜

--- ls_Toasts/core/changelog.lua
+++ ls_Toasts/core/changelog.lua
@@ -6,5 +6,9 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-- Fixed "[DNT] Worldsoul Memory Score" toasts.
+- Added a workaround for buggy guild achievement spam. If your character is in a guild, the addon will add already
+  completed guild achievements to the blacklist on log in, you still will be able to see guild achievements if they're
+  earned for the first time. If you join a guild at some point, reload the UI, the achievement API is super laggy, so I
+  avoid rebuilding the blacklist during the actual game session.
+- Added an option to disable tooltips on mouseover.
 ]]
