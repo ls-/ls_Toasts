991d315e59546f0855098c7cb6166704ff077d7f
Val Voronov <i.lightspark@gmail.com>
2024-09-28 17:10:02 +0700

    📜

--- ls_Toasts/core/changelog.lua
+++ ls_Toasts/core/changelog.lua
@@ -6,10 +6,6 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-- Reworked the workaround for the guild achievement spam. After further investigating the issue it became apparent that
-  it's impossible to reliably automate it without affecting newer smaller guilds that might want to see toasts for these
-  achievements. For this reason I'm adding a toggle to enable filtering for guild achievements if your guild is
-  experiencing it. This will block toasts for a bunch of faction-specific PvP and reputation achievements. Can be found
-  at /LST > Toast Types > Achievement > Filter Guild Achievements, disabled by default.
-- Updated Traditional Chinese translation. Translated by BNS333@Curse.
+- Removed the workaround for the guild achievement spam. It's finally fixed by Blizzard. An ancient bug that had been
+  ignored for years got fixed asap after it showed up multiple times during RWF streams and caused a few wipes.
 ]]
