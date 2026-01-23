233aea861cc0c9fd87d38aee04877aa093deb2b2
Val Voronov <i.lightspark@gmail.com>
2026-01-23 12:32:05 +0700

    💄

--- ls_Toasts/systems/loot_special.lua
+++ ls_Toasts/systems/loot_special.lua
@@ -384,11 +384,11 @@ E:RegisterOptions("loot_special", {
 			type = "select",
 			name = L["LOOT_THRESHOLD"],
 			values = {
-				[1] = ITEM_QUALITY_COLORS[1].hex .. ITEM_QUALITY1_DESC .. "|r",
-				[2] = ITEM_QUALITY_COLORS[2].hex .. ITEM_QUALITY2_DESC .. "|r",
-				[3] = ITEM_QUALITY_COLORS[3].hex .. ITEM_QUALITY3_DESC .. "|r",
-				[4] = ITEM_QUALITY_COLORS[4].hex .. ITEM_QUALITY4_DESC .. "|r",
-				[5] = ITEM_QUALITY_COLORS[5].hex .. ITEM_QUALITY5_DESC .. "|r",
+				[1] = ITEM_QUALITY_COLORS[1].hex .. _G.ITEM_QUALITY1_DESC .. "|r",
+				[2] = ITEM_QUALITY_COLORS[2].hex .. _G.ITEM_QUALITY2_DESC .. "|r",
+				[3] = ITEM_QUALITY_COLORS[3].hex .. _G.ITEM_QUALITY3_DESC .. "|r",
+				[4] = ITEM_QUALITY_COLORS[4].hex .. _G.ITEM_QUALITY4_DESC .. "|r",
+				[5] = ITEM_QUALITY_COLORS[5].hex .. _G.ITEM_QUALITY5_DESC .. "|r",
 			},
 		},
 		legacy_equipment = {
