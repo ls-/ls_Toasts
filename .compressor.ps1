c9b7f5706fb9d138bc4c7e6ab5ecabbfb0d82167
Val Voronov <i.lightspark@gmail.com>
2026-05-13 19:08:55 +0200

    🛠️

--- .compressor.ps1
+++ .compressor.ps1
@@ -48,7 +48,7 @@ if (Test-Path $temp) {
 
 New-Item -Path $temp -ItemType Directory | Out-Null
 Copy-Item $foldersToInclude -Destination $temp -Exclude $filesToExclude -Recurse
-Get-ChildItem "$temp\$name\embeds\" -Recurse | Where-Object { $_.PSIsContainer -and $_.Name -cin $foldersToRemove } | Remove-Item -Recurse -Force
+Get-ChildItem "$temp\$name\embeds\" -Attributes Hidden, !Hidden -Recurse | Where-Object { $_.PSIsContainer -and $_.Name -cin $foldersToRemove } | Remove-Item -Recurse -Force
 7zz a -tzip -mx9 "$name-$version.zip" (Get-ChildItem $temp)
 Move-Item "$name-$version.zip" -Destination .. -Force
 Remove-Item $temp -Recurse -Force
