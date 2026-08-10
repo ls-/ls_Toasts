d46bcabf01a8debacddef4e7c33fa101ecdd8255
Val Voronov <i.lightspark@gmail.com>
2026-08-11 00:01:37 +0200

    Update WQ toasts

--- ls_Toasts/systems/world.lua
+++ ls_Toasts/systems/world.lua
@@ -56,7 +56,7 @@ local function Toast_SetUp(event, isUpdate, questID, name, moneyReward, xpReward
 			end
 		end
 
-		if xpReward and xpReward > 0 and UnitLevel("player") < GetMaxPlayerLevel() then
+		if xpReward and xpReward > 0 and not IsXPUserDisabled() and not GameRulesUtil.IsPlayerAtEffectiveMaxLevel() then
 			usedSlots = usedSlots + 1
 
 			local slot = toast["Slot" .. usedSlots]
